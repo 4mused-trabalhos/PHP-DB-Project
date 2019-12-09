f3612c19679fb9716955eaf17201ffaba6409f71
Pedro Gomes <pedrogomes2000.3@gmail.com>
2019-12-09 22:04:19 +0000

    Basic html syntax written

---
+++ index.php
@@ -0,0 +1,37 @@
+<html lang="en">
+
+    <head>
+
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+        <title>PHP Project</title>
+        <link rel="icon" href="#">
+
+        <link rel="stylesheet" href="assets\fonts\inter\inter.css">
+        <link rel="stylesheet" href="css\styles.css">
+
+    </head>
+
+    <body>
+
+        <?php
+        
+            include_once "include\connection.inc.php";
+        
+        ?>
+
+        <script src="js\etc.js"></script>
+
+    </body>
+
+    <!--
+
+        Trabalho realizado por:
+            Bruno Ascenção nº. 3;
+            Pedro Gomes nº. 17;    
+        
+    !-->
+
+</html>
